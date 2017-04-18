6d900e1beeb5ddb439b0f3266fc8d50574989c23
Your Name <you@example.com>
2017-04-18 10:55:28 -0500

    mas funcionalidaaaad

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
 var cerrar= [];
+var foto = [];
 var restaurar = document.getElementById("fotosPandas")
 
 for(var i=0 ; i<4 ;i++){
-  cerrar[i]=document.getElementsByClassName("img")[i];
+  foto[i]= document.getElementsByClassName("img")[i];
+  cerrar[i]=document.getElementsByClassName("close")[i];
   cerrar[i].addEventListener("click", cerrarFoto)
 }
 
 restaurar.addEventListener("click",restaurar);
 
 function cerrarFoto(){
-  cerrar.style.display ="none"
+  cerrar.style.display = "none";
 }
 
 function restaurar(){
   restaurar.style.display="inline-blocks";
 }
-
-console.log("hijaso")
