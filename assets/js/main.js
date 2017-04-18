5b6a4eee825031373cfc2eb20bc8a7d56b43bb37
Your Name <you@example.com>
2017-04-18 11:06:36 -0500

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
 var cerrar= [];
 var foto = [];
+
 var restaurar = document.getElementById("fotosPandas")
+var texto2 = document.getElementsByClassName("articulos")[1];
+var btnExtincion = document.getElementsByClassName("barra")[1];
+var btnOrigen = document.getElementsByClassName("barra")[0];
 
 for(var i=0 ; i<4 ;i++){
   foto[i]= document.getElementsByClassName("img")[i];
@@ -9,11 +13,22 @@ for(var i=0 ; i<4 ;i++){
 }
 
 restaurar.addEventListener("click",restaurar);
+btnExtincion.addEventListener("click", suprimirTexto);
+btnOrigen.addEventListener("click",restaurarTexto)
 
 function cerrarFoto(){
   cerrar.style.display = "none";
+  foto.style.display="none";
 }
 
 function restaurar(){
   restaurar.style.display="inline-blocks";
+}
+
+function suprimirTexto(){
+  texto.style.display = "none";
+}
+
+function restaurarTexto(){
+  texto.style.display = "inline-block"
 }
